dd87ec3f71f0d2ace06c9d47edbb26ec888057e4
Daniel Suarez <suarez.daniel101@gmail.com>
2021-11-15 23:22:08 -0500

    commit tree 2b5a1fa67fbe7c63df44f789d6e0c05536db82f0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,42 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/articles' do
+    @articles = Article.all
+    erb :'articles/index'
   end
+
+  get '/articles/new' do 
+    erb :'articles/new'
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params["id"])
+    erb :'articles/show'
+  end
+
+  post '/articles' do
+    article = Article.create(title: params[:article][:title], content: params[:article][:content])
+    redirect to "/articles/#{article.id}"
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params["id"])
+    erb :'articles/edit'
+  end
+
+  patch '/articles/:id' do
+    # @article = Article.find_by(id: params[:id])
+    @article = Article.find(params["id"])
+    @article.title = params[:title]
+    @article.content = params[:content]
+    @article.save
+    erb :'articles/show'
+  end
+
+  delete '/articles/:id' do
+    @article = Article.delete(params["id"])
+    redirect to("/articles")
+  end 
 end
+
